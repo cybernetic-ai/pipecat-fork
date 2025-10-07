eb2418a83b34336aa98a39ba84f4b47a88f8f705
Ajay Arora <ajayarora1235@gmail.com>
2025-10-07 09:13:10 -0700

    also get rid of deepgram logging here

--- src/pipecat/services/stt_service.py
+++ src/pipecat/services/stt_service.py
@@ -152,12 +152,12 @@ class STTService(AIService):
         else:
             self._user_id = ""
 
-        if not frame.audio:
-            # Ignoring in case we don't have audio to transcribe.
-            logger.warning(
-                f"Empty audio frame received for STT service: {self.name} {frame.num_frames}"
-            )
-            return
+        # if not frame.audio:
+        #     # Ignoring in case we don't have audio to transcribe.
+        #     logger.warning(
+        #         f"Empty audio frame received for STT service: {self.name} {frame.num_frames}"
+        #     )
+        #     return
 
         await self.process_generator(self.run_stt(frame.audio))
 
