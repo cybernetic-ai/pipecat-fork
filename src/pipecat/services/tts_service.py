d6af2771d2060c36416d66e30bb77a6205adf4e0
Ajay Arora <ajayarora1235@gmail.com>
2025-10-09 00:09:27 -0700

    Revert "Merge pull request #2 from cybernetic-ai/aa/remove-duplicate-tts-frames"
    This reverts commit fd077cec7a8cf84ed5ce19873854f9e5c54c8b6a, reversing
    changes made to 9addf3b5281a76ada4a199cdab74e7973ce266e5.

--- src/pipecat/services/tts_service.py
+++ src/pipecat/services/tts_service.py
@@ -405,15 +405,10 @@ class TTSService(AIService):
 
         await self.stop_processing_metrics()
 
-
-        # NOTE: This is disabled because
-        # We already push the text frames in the word handler.
-        # And do not need to push them again here.
-        
-        # if self._push_text_frames:
-        #     # We send the original text after the audio. This way, if we are
-        #     # interrupted, the text is not added to the assistant context.
-        #     await self.push_frame(TTSTextFrame(text))
+        if self._push_text_frames:
+            # We send the original text after the audio. This way, if we are
+            # interrupted, the text is not added to the assistant context.
+            await self.push_frame(TTSTextFrame(text))
 
     async def _stop_frame_handler(self):
         has_started = False
